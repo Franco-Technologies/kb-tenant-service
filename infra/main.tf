950dd27350a77dd5775689a6ad9c28fbea9eee01
Gabriel Franco <francotechnologiesllc@gmail.com>
2024-07-31 15:47:29 -0500

    update vars used

--- infra/main.tf
+++ infra/main.tf
@@ -6,12 +6,12 @@ locals {
 module "ecs" {
   source = "./modules/ecs"
   # Environment-specific variables
-  env               = local.app_vars_decoded.env
+  env               = local.env
   vpc_id            = local.app_vars_decoded.vpc_id
-  cluster_arn       = local.app_vars_decoded.cluster_arn
-  subnet_ids        = local.app_vars_decoded.subnet_ids
-  container_image   = local.app_vars_decoded.container_image
-  security_group_id = local.app_vars_decoded.security_group_id
+  cluster_arn       = local.app_vars_decoded.ecs_cluster_arn
+  subnet_ids        = local.app_vars_decoded.private_subnet_ids
+  container_image   = "nginx:latest"
+  security_group_id = local.app_vars_decoded.default_security_group_id
   listener_arn      = local.app_vars_decoded.listener_arn
 }
 
@@ -20,9 +20,9 @@ module "api_gateway" {
   # Environment-specific variables
   stage_name        = local.env
   vpc_link_name     = "ecs-vpc-link"
-  load_balancer_url = local.app_vars_decoded.load_balance_url
-  rest_api_id       = local.app_vars_decoded.rest_api_id
-  root_resource_id  = local.app_vars_decoded.root_resource_id
+  load_balancer_url = local.app_vars_decoded.load_balancer_arn
+  rest_api_id       = local.app_vars_decoded.api_gateway_rest_api_id
+  root_resource_id  = local.app_vars_decoded.api_gateway_root_resource_id
   tags = {
     Environment = "dev"
     Project     = "example"
