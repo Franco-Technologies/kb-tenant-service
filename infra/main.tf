8189544c3b9cea7f6adc24d2e1afcc8c0c328950
Gabriel Franco <francotechnologiesllc@gmail.com>
2024-07-31 15:49:02 -0500

    update subnet format

--- infra/main.tf
+++ infra/main.tf
@@ -9,7 +9,7 @@ module "ecs" {
   env               = local.env
   vpc_id            = local.app_vars_decoded.vpc_id
   cluster_arn       = local.app_vars_decoded.ecs_cluster_arn
-  subnet_ids        = local.app_vars_decoded.private_subnet_ids
+  subnet_ids        = jsondecode(local.app_vars_decoded.private_subnet_ids)
   container_image   = "nginx:latest"
   security_group_id = local.app_vars_decoded.default_security_group_id
   listener_arn      = local.app_vars_decoded.listener_arn
