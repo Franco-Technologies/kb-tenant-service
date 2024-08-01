3ca3c0d1d203893b9c4df95824f380cb9c9babfa
Gabriel Franco <francotechnologiesllc@gmail.com>
2024-08-01 12:27:46 -0500

    update image

--- src/api/app.js
+++ src/api/app.js
@@ -3,10 +3,12 @@ const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
+  console.log("Hello World!!");
   res.send("Hello World!!");
 });
 
 app.get("/api", (req, res) => {
+  console.log("API");
   res.json({ message: "s" });
 });
 
