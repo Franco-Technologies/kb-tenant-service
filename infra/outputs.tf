c5fb904fc9d611e4e4e49ecda7f5e29b3e15c07c
Gabriel Franco <francotechnologiesllc@gmail.com>
2024-07-31 20:51:30 -0500

    destroy gw

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -3,13 +3,13 @@ output "ecs_service_url" {
   value       = module.ecs.service_url
 }
 
-output "api_gateway_deployment_id" {
-  description = "API Gateway Deployment ID"
-  value       = module.api_gateway.deployment_id
-}
+# output "api_gateway_deployment_id" {
+#   description = "API Gateway Deployment ID"
+#   value       = module.api_gateway.deployment_id
+# }
 
-output "api_gateway_vpc_link_id" {
-  description = "API Gateway VPC Link ID"
-  value       = module.api_gateway.vpc_link_id
-}
+# output "api_gateway_vpc_link_id" {
+#   description = "API Gateway VPC Link ID"
+#   value       = module.api_gateway.vpc_link_id
+# }
 # Add other relevant outputs
